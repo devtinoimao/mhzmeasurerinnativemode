eebc5cc0458132c454558f610138808272961610
devtinoimao <110640733+devtinoimao@users.noreply.github.com>
2026-01-17 20:30:19 +0200

    Implement CPU speed measurement in MHz
    Added a loop to measure CPU speed in MHz and display results.

--- buildfolder/mhz.c
+++ buildfolder/mhz.c
@@ -1,4 +1,3 @@
-// Basic types for the compiler
 typedef void* PVOID;
 typedef void* HANDLE;
 typedef long NTSTATUS;
@@ -6,48 +5,69 @@ typedef unsigned short WCHAR;
 typedef unsigned long ULONG;
 typedef unsigned __int64 UINT64;
 
-// The structure Windows uses for strings in Native Mode
 typedef struct _UNICODE_STRING {
     unsigned short Length;
     unsigned short MaximumLength;
     WCHAR* Buffer;
 } UNICODE_STRING, *PUNICODE_STRING;
 
-// Function prototypes - linked from ntdll.lib
+// ntdll imports
 __declspec(dllimport) NTSTATUS __stdcall NtDisplayString(PUNICODE_STRING String);
 __declspec(dllimport) NTSTATUS __stdcall NtTerminateProcess(HANDLE ProcessHandle, NTSTATUS ExitStatus);
 __declspec(dllimport) void __stdcall RtlInitUnicodeString(PUNICODE_STRING DestinationString, const WCHAR* SourceString);
 
-// Simple loop-based delay for the measurement window
-void SimpleDelay() {
-    for (volatile int i = 0; i < 100000000; i++); 
+// Simple Integer to WCHAR conversion (since we can't use swprintf)
+void IntToWStr(UINT64 n, WCHAR* str) {
+    WCHAR temp[20];
+    int i = 0, j = 0;
+    if (n == 0) temp[i++] = L'0';
+    while (n > 0) {
+        temp[i++] = (n % 10) + L'0';
+        n /= 10;
+    }
+    while (i > 0) str[j++] = temp[--i];
+    str[j] = L'\0';
 }
 
-// The Entry Point (Replaces main)
+// Entry point for Native Subsystem
 void NtProcessStartup(PVOID StartupArgument) {
     UNICODE_STRING msg;
-    UINT64 start, end;
+    UINT64 start, end, mhz;
+    WCHAR mhz_buf[64];
+    WCHAR final_msg[128];
     
-    // 1. Greet the user
-    RtlInitUnicodeString(&msg, L"GEMINI NATIVE MHZ MEASURER\nStarting calibration...\n");
+    // 1. Initial Greeting
+    RtlInitUnicodeString(&msg, L"\nXP NATIVE FREQUENCY MONITOR\n---------------------------\n");
     NtDisplayString(&msg);
 
-    // 2. Measure Cycles
-    start = __rdtsc();
-    SimpleDelay();
-    end = __rdtsc();
-    
-    // 3. Simple logic to show we did something
-    // (Note: Formatting integers to strings in Native mode usually requires 
-    // custom code, so we'll print a success message first)
-    if (end > start) {
-        RtlInitUnicodeString(&msg, L"Measurement Complete. CPU is alive!\nExiting to Windows...");
+    // 2. The Measurement Loop
+    // We will run this in a loop so it keeps updating until you "stop" it
+    for(int loops = 0; loops < 10; loops++) {
+        start = __rdtsc();
+        // Delay loop for approx 1 second
+        for (volatile int i = 0; i < 200000000; i++); 
+        end = __rdtsc();
+
+        mhz = (end - start) / 1000000;
+
+        // Convert and Display
+        IntToWStr(mhz, mhz_buf);
+        
+        // Manual string concatenation for the display
+        RtlInitUnicodeString(&msg, L"Current Speed: ");
+        NtDisplayString(&msg);
+        RtlInitUnicodeString(&msg, mhz_buf);
+        NtDisplayString(&msg);
+        RtlInitUnicodeString(&msg, L" MHz\n");
         NtDisplayString(&msg);
     }
 
-    // 4. Give the user 3 seconds to read before Windows takes over
-    for (volatile int i = 0; i < 500000000; i++);
+    // 3. Exit sequence
+    RtlInitUnicodeString(&msg, L"\nMeasurement finished. Returning to Windows XP...\n");
+    NtDisplayString(&msg);
 
-    // 5. Hand control back to the Session Manager
+    // Final short pause so you can see the last result
+    for (volatile int i = 0; i < 300000000; i++);
+
     NtTerminateProcess((HANDLE)-1, 0);
 }
